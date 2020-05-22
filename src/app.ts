a9ef4d6a68a23c3cf7e3b9a4d84f373889a76ec2
Khris <furlongoliver@gmail.com>
2020-05-22 17:42:26 +0100

    Finished the rewrite

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
 import * as crypto from "crypto";
 import * as fs from "fs-extra";
-const countLinesInFile = require('count-lines-in-file');
+import { terminal } from "terminal-kit";
+import * as math from 'mathjs'
 
-
+const countLines = require('count-lines-in-file')
 //MD5 Hash Fuction
 function MD5(tohash: string) {
     return (crypto.createHash('MD5').update(tohash).digest("hex"))
@@ -93,14 +94,141 @@ function hashFromfileNoBuffer(path: string, hashtype: string, savePath: string, 
             }
         });
     }).on('end', function () {
-        console.log('done')
+
+
+        terminal.green("\nHashing Complete")
+        uiAgain()
+    });
+}
+// fucntion for the file input for the ui
+function uiInputFile() {
+    terminal('Choose input file: ');
+    terminal.fileInput(
+        { baseDir: './' },
+        function (error: Error, path: string) {
+            if (error) {
+                terminal.red.bold("\nAn error occurs: " + error + "\n");
+            }
+            else {
+                //file validation checks the path exits and checks to see if it is a file rather than a dir
+                if (fs.pathExists(path) && fs.lstatSync(path).isFile()) {
+                    terminal.green("\n Input file is '%s'\n", path);
+                    uiOutputFile(path)
+                } else {
+                    terminal.red.bold('\nFile cound not be found. Check if file exsits.\n')
+                    uiInputFile()
+                }
+
+            }
+        }
+    );
+}
+function uiOutputFile(path: string) {
+    terminal('Choose output file: ');
+    terminal.fileInput(
+        { baseDir: './' },
+        function (error: Error, savePath: string) {
+            if (error) {
+                terminal.red.bold("\nAn error occurs: " + error + "\n");
+            }
+            else {
+                terminal.green("\nYour file is '%s'", savePath);
+                hashSelector(path, savePath)
+            }
+        }
+    );
+
+}
+const supportedHashes = [
+    'MD5',
+    'SHA1',
+    'SHA256',
+    'SHA224',
+    'SHA512',
+    'SHA384',
+    'SHA3',
+    'RIPEMD160'
+];
+
+function hashSelector(path: string, savePath: string) {
+    terminal('\nSelect Hashing Algorithm')
+    terminal.singleLineMenu(supportedHashes, { style: terminal.inverse, }, function (error, response) {
+        terminal('\n').eraseLineAfter.green("Selected: %s\n", response.selectedText);
+        setDelimiter(path, savePath, response.selectedText)
     });
 }
 
+function setDelimiter(path: string, savePath: string, hash: string) {
+    terminal('Please enter the Delimiter to be used: \n');
+    terminal.inputField(
+        { autoComplete: [','], autoCompleteMenu: true },
+        function (error, delimiter) {
+            if (!delimiter) delimiter = ','
+            terminal.green("\nDelimiter: %s\n", delimiter);
+            setLineBuffer(path, savePath, hash, delimiter)
+        }
+    );
+}
+function setLineBuffer(path: string, savePath: string, hash: string, delimiter: string) {
+
+    countLines(path, (error: Error, linenumebrs: number) => {
+        console.log(linenumebrs)
+        let arraylinenum: Array<any> = [
+            math.round(linenumebrs).toString(),
+            math.round(linenumebrs / 2).toString(),
+            math.round(linenumebrs / 4).toString(),
+            '1'
+        ]
+        terminal('Select the number of lines to buffer before writing to file. (Higher is usualy faster but with a higher memeory cost)')
+
+        terminal.singleLineMenu(arraylinenum, { style: terminal.inverse, }, function (error, response) {
+            terminal('\n').eraseLineAfter.green(
+                "Selected: %s\n",
+                response.selectedText,
+            );
+            uiFinalCheck(path, hash, savePath, delimiter, parseInt(response.selectedText))
+        });
+    });
+
+}
+
+function uiFinalCheck(path: string, savePath: string, hash: string, delimiter: string, buffer: number) {
+    terminal.bold(`Input Path: ${path}\n Output Path ${savePath}\n Hashing Algorithm: ${hash}\n Delimiter: ${delimiter}\n Buffer: ${buffer}\n`).noFormat(`Are theses details correct? [Y,n]`)
+    terminal.yesOrNo({ yes: ['y', 'ENTER'], no: ['n'] }, function (error, result) {
+
+        if (result) {
+            terminal.green("\nStarting Hashing");
+            hashFromfileNoBuffer(path, savePath, hash, delimiter, buffer)
+        }
+        else {
+            uiInputFile()
+        }
+    });
+}
+function uiAgain() {
+    terminal("\nHash Again? [Y/n]\n")
+    terminal.yesOrNo({ yes: ['y', 'ENTER', 'Y'], no: ['n'] }, function (error, result) {
+
+        if (result) {
+            terminal.clear()
+            uiInputFile()
+        }
+        else {
+            process.exit()
+        }
+    })
+}
 
 try {
+    uiInputFile()
+} catch (error) {
+    console.log(error)
+}
+
+
+/* try {
     hashFromfileNoBuffer("./examples/10-million-password-list-top-1000000.txt", "SHA512", "./examples/hashes.csv", '/', 100000)
 
 } catch (err) {
     console.error(err)
-}
+} */
